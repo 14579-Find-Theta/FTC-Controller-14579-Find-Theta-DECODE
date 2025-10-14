1ca430a040100538b58dc7ee89e9128453eab110
Henwyyyy <127368464+skushy91@users.noreply.github.com>
2025-10-14 15:55:52 +1000

    Shooter started v2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Shooter.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Shooter {
@@ -15,13 +16,14 @@ public class Shooter {
 
     Shooter (HardwareMap map) {
         shooterMotor = map.get(DcMotor.class, "shooterMotor");
-
+        shooterMotor.setDirection(REVERSEMOTOR ? DcMotorSimple.Direction.REVERSE : DcMotorSimple.Direction.FORWARD);
         shooterServo1 = map.get(CRServo.class, "shooterServo1");
         shooterServo2 = map.get(CRServo.class, "shooterServo2");
+
     }
 
     void outtakeShoot (double RTrigger) {
-        if (Math.abs(RTrigger) > 0.2) {
+        if (Math.abs(RTrigger) > 0.1) {
             shooterMotor.setPower(LTrigger-RTrigger);
         } else {
             intakeServo.setPower(0);
